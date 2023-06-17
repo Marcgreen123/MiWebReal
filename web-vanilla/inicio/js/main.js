e293ca6a1cbad5912036d0831733bc26ed947fbc
Marcos Monteverde <marcosdelingles@gmail.com>
2023-06-16 23:56:41 -0300

    agregue animaciones al scrollear

--- web-vanilla/inicio/js/main.js
+++ web-vanilla/inicio/js/main.js
@@ -47,6 +47,31 @@ function scrollDown(){
 //Generar codigo para que aparezcan los elementos con una animacion al scrollear
 
 
+ScrollReveal().reveal('.animate', {
+    duration: 600, // Duración de la animación en milisegundos
+    distance: '20px', // Distancia desde la que se revela el elemento
+    origin: 'bottom', // Origen de la animación (puedes usar: 'top', 'right', 'bottom', 'left')
+    easing: 'ease-out', // Efecto de aceleración
+    interval: 200 // Intervalo de tiempo entre cada elemento
+});
+
+ScrollReveal().reveal('.animate2', {
+    duration: 1000, // Duración de la animación en milisegundos
+    distance: '20px', // Distancia desde la que se revela el elemento
+    origin: 'left', // Origen de la animación (puedes usar: 'top', 'right', 'bottom', 'left')
+    easing: 'ease-out', // Efecto de aceleración
+    interval: 400 // Intervalo de tiempo entre cada elemento
+});
+
+ScrollReveal().reveal('.animate3', {
+    duration: 1000, // Duración de la animación en milisegundos
+    distance: '20px', // Distancia desde la que se revela el elemento
+    origin: 'right', // Origen de la animación (puedes usar: 'top', 'right', 'bottom', 'left')
+    easing: 'ease-out', // Efecto de aceleración
+    interval: 400 // Intervalo de tiempo entre cada elemento
+});
+
+
 
 
 /*
